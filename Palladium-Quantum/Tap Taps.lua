ce6acf28d904d846bd330e3393cd13335e252272
Michael Newell <throwaway480085@gmail.com>
2021-10-14 22:50:13 -0700

    Fixed mistakes with Palladium-Quantum notes

--- Palladium-Quantum/Tap Taps.lua
+++ Palladium-Quantum/Tap Taps.lua
@@ -1,16 +1,18 @@
 local sButton = Var "Button"
 local sEffect = Var "Effect"
 local sPlayer = Var "Player"
+local sColor = Var "Color"
+if sColor == "" then sColor = "4th" end
 
 return Def.ActorFrame {
     Def.Model { --fever note color
         InitCommand=function(self)
             if string.find(sButton, "Strum") then
-                local color1 = color(PalladiumColorTable[sButton])
-                local color2 = color(PalladiumColorTable["Fret 6"])
+                local color1 = color(PalladiumQuantumColorTable[sColor])
+                local color2 = color(PalladiumQuantumColorTable["Fever"])
                 self:rotationx(90):diffuse((color1[1] + color2[1]) * 0.5, (color1[2] + color2[2]) * 0.5, (color1[3] + color2[3]) * 0.5, 1)
             else
-                self:rotationx(90):diffuse(color(PalladiumColorTable[sButton]))
+                self:rotationx(90):diffuse(color(PalladiumQuantumColorTable[sColor]))
             end
             if tonumber(sEffect) <= 0 then
                 self:diffusealpha(0)
@@ -23,25 +25,6 @@ return Def.ActorFrame {
         Meshes=(tonumber(sEffect) <= 0 and "non.txt") or (string.find(sButton, "Strum") and "strum color.txt") or "star color.txt",
         Materials=(string.find(sButton, "Strum") and "resource/Strum mats.txt") or "resource/Fret taps mats.txt",
         Bones="star color.txt",
-        FeverMessageCommand=function(self,params)
-            if params.pn ~= sPlayer then return end
-            if tonumber(sEffect) > 0 then
-                if params.Active then
-                    self:diffuse(color(PalladiumColorTable["Fret 6"]))
-                else
-                    if string.find(sButton, "Strum") then
-                        local color1 = color(PalladiumColorTable[sButton])
-                        local color2 = color(PalladiumColorTable["Fret 6"])
-                        self:diffuse((color1[1] + color2[1]) * 0.5, (color1[2] + color2[2]) * 0.5, (color1[3] + color2[3]) * 0.5, 1)
-                    else
-                        self:diffuse(color(PalladiumColorTable[sButton]))
-                    end
-                end
-                if self.isHidden then
-                    self:diffusealpha(0)
-                end
-            end
-        end,
         FeverMissedMessageCommand=function(self,params)
             if params.pn ~= sPlayer then return end
             if tonumber(sEffect) > 0 then
@@ -57,7 +40,7 @@ return Def.ActorFrame {
     },
     Def.Model { --fever note glow
         InitCommand=function(self)
-            self:rotationx(90):diffuse(color(PalladiumColorTable["Fret 6"]))
+            self:rotationx(90):diffuse(color(PalladiumQuantumColorTable["Fever"]))
             if tonumber(sEffect) <= 0 then
                 self:diffusealpha(0)
                 self.isHidden = true
@@ -69,15 +52,6 @@ return Def.ActorFrame {
         Meshes=(tonumber(sEffect) <= 0 and "non.txt") or (string.find(sButton, "Strum") and "strum.txt") or "star glow.txt",
         Materials=(string.find(sButton, "Strum") and "resource/Strum hopo mats.txt") or "resource/Fret mats.txt",
         Bones="star glow.txt",
-        FeverMessageCommand=function(self,params)
-            if params.pn ~= sPlayer then return end
-            if tonumber(sEffect) > 0 then
-                self:diffuse(color(PalladiumColorTable["Fret 6"]))
-                if self.isHidden then
-                    self:diffusealpha(0)
-                end
-            end
-        end,
         FeverMissedMessageCommand=function(self,params)
             if params.pn ~= sPlayer then return end
             if tonumber(sEffect) > 0 then
@@ -120,7 +94,7 @@ return Def.ActorFrame {
     },
     Def.Model { --regular note color
         InitCommand=function(self)
-            self:rotationx(90):diffuse(color(PalladiumColorTable[sButton]))
+            self:rotationx(90):diffuse(color(PalladiumQuantumColorTable[sColor]))
             if tonumber(sEffect) > 0 then
                 self:diffusealpha(0)
                 self.isHidden = true
@@ -131,17 +105,6 @@ return Def.ActorFrame {
         Meshes=string.find(sButton, "Strum") and "strum color.txt" or "gem color.txt",
         Materials=(string.find(sButton, "Strum") and "resource/Strum mats.txt") or "resource/Fret taps mats.txt",
         Bones="gem color.txt",
-        FeverMessageCommand=function(self,params)
-            if params.pn ~= sPlayer then return end
-            if params.Active then
-                self:diffuse(color(PalladiumColorTable["Fret 6"]))
-            else
-                self:diffuse(color(PalladiumColorTable[sButton]))
-            end
-            if self.isHidden then
-                self:diffusealpha(0)
-            end
-        end,
         FeverMissedMessageCommand=function(self,params)
             if params.pn ~= sPlayer then return end
             if tonumber(sEffect) > 0 then
